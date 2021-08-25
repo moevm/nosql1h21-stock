7817ba71519f4bfe3f16dd3b72aa86c45bc2df70
AlexRyzhickov <makseljoinb@gmail.com>
2021-08-25 23:21:09 +0300

    add cors, change port

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Config struct {
-	Port             int    `env:"PORT" envDefault:"8080"`
-	DBConnString     string `env:"DB_CONN_STRING" envDefault:"mongodb://127.0.0.1"`
+	Port         int    `env:"PORT" envDefault:"3000"`
+	DBConnString string `env:"DB_CONN_STRING" envDefault:"mongodb://127.0.0.1"`
 }
 
 func New() (*Config, error) {
