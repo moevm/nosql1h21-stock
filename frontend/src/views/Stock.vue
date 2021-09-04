560a96d368e9eeb35f15035d862152e5ddb117cd
Heliconter <25860895+Heliconter@users.noreply.github.com>
2021-09-04 18:16:35 +0300

    frontend: Adding the finantial data into the stock view

--- frontend/src/views/Stock.vue
+++ frontend/src/views/Stock.vue
@@ -2,11 +2,23 @@
   <h2>Stock</h2>
   <Spinner v-if="!stock"/>
   <div v-else class="stock">
-    <p>Name: {{ stock.ShortName }}</p>
     <p>Ticker: {{ stock.Symbol }}</p>
+    <p>Name: {{ stock.ShortName }}</p>
     <p>Sector: {{ stock.Sector }}</p>
     <p>Industry: {{ stock.Industry }}</p>
     <p>Summary: {{ stock.Summary }}</p>
+    <p>Country: {{ stock.Locate.Country }}</p>
+    <p>Total cash: {{stock.FinancialData.TotalCash}}</p>
+    <p>Total cash per share: {{stock.FinancialData.TotalCashPerShare}}</p>
+    <p>Ebitda: {{stock.FinancialData.Ebitda}}</p>
+    <p>Total debt: {{stock.FinancialData.TotalDebt}}</p>
+    <p>Quick ratio: {{stock.FinancialData.QuickRatio}}</p>
+    <p>Current ratio: {{stock.FinancialData.CurrentRatio}}</p>
+    <p>Total revenue: {{stock.FinancialData.TotalRevenue}}</p>
+    <p>Revenue per share: {{stock.FinancialData.RevenuePerShare}}</p>
+    <p>Debt to equity: {{stock.FinancialData.DebtToEquity}}</p>
+    <p>Return on assets: {{stock.FinancialData.ReturnOnAssets}}</p>
+    <p>Return on equity: {{stock.FinancialData.ReturnOnEquity}}</p>
   </div>
 </template>
 
