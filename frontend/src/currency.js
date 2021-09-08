276afc33fd62a3e2dde7cd22dfc5b550e8a92147
Heliconter <25860895+Heliconter@users.noreply.github.com>
2021-09-09 00:44:37 +0300

    Fixing the currency formatting with negative amount

--- frontend/src/currency.js
+++ frontend/src/currency.js
@@ -2,7 +2,7 @@ export function currency(amount, name) {
     let factors = ["m", "b", "t"]
     let factor = ""
     for (let i = 0; i < factors.length; i++) {
-        if (amount < 10000)
+        if (Math.abs(amount) < 10000)
             break
         amount = Math.round(amount)
         amount /= 1000
